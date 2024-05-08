bdfb1cc7411cd06ece501fa94af12116e66fdc8e
Firadi <Firadi.firadi@gmail.com>
2024-05-08 16:13:31 +0100

    fixing drag and drop zone

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,14 +1,22 @@
 import { Component, Input, inject } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { DragDropDirective, FileHandle } from '../drag-drop.directive';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
 	selector: 'ngbd-modal-content',
 	standalone: true,
 	templateUrl: "./modal.component.html",
 	styleUrl: './modal.component.scss',
+	imports: [DragDropDirective, NgIf, NgFor]
 })
 export class ModalComponent {
   activeModal = inject(NgbActiveModal);
+  @Input() name: string;
+  files: FileHandle[] = [];
+  test = true;
 
-	@Input() name: string;
+  filesDropped(files: FileHandle[]): void {
+    this.files = files;
+  }
 }

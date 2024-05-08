f2131153e5250f187377434af2d6694c801f1cea
oussama <oussamaalaouu@gmail.com>
2024-05-08 13:10:29 +0100

    final last page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StartComponent } from './start/start.component';
+import { DisplayDataComponent } from './display-data/display-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppComponent, StartComponent],
+  imports: [RouterOutlet, AppComponent, StartComponent, DisplayDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

75e034f746d2778635610a7cec8cee5bd0d83c6f
Firadi <Firadi.firadi@gmail.com>
2024-05-11 16:39:48 +0100

    add keyboard shortcuts to image controls

--- src/app/directives/zoom-image.directive.ts
+++ src/app/directives/zoom-image.directive.ts
@@ -8,9 +8,9 @@ export class ZoomImageDirective {
   
   @Input() width: number = 500;
 
-  @Input() zoomMax: number = 700;
+  @Input() zoomMax: number = 900;
 
-  @Input() zoomMin: number = 100;
+  @Input() zoomMin: number = 250;
 
   @Input('appZoomImage') zoomDirection: 'zoom-in' | 'zoom-out' | 'rotate-right' | 'rotate-left' = 'zoom-in'; 
 
@@ -19,29 +19,56 @@ export class ZoomImageDirective {
   constructor() { }
 
   @HostListener('click') onClick() {
-      this.width = parseInt(this.zoomTarget.style.width) ? parseInt(this.zoomTarget.style.width) : 700;
-      if (this.zoomDirection === 'zoom-in' && this.zoomMax > this.width ) {
-        this.width += 25;
-        this.zoom(this.width);
-        console.log("in");
-      }else if (this.zoomDirection === 'zoom-out' && this.width > this.zoomMin) {
-        this.width -= 25;
-        this.zoom(this.width);
-        console.log("out");
-      }else {
-        this.rotate(this.zoomDirection)
-      }
-      
+    this.applyZoom();  
   }
 
-  private zoom(width: number){
-    this.zoomTarget.style.width = width + 'px';
+  @HostListener('document:keydown', ['$event']) onKeyDown(event: KeyboardEvent) {
+    if (event.key === '+' || event.key === '=') {
+      this.zoomIn();
+    } else if (event.key === '-' || event.key === '_') {
+      this.zoomOut();
+    }
   }
+
+  private applyZoom(): void {
+    
+    if (this.zoomDirection === 'zoom-in' && this.width < this.zoomMax) {
+      this.width += 45;
+      this.zoom(this.width);
+    } else if (this.zoomDirection === 'zoom-out' && this.width > this.zoomMin) {
+      this.width -= 45;
+      this.zoom(this.width);
+    } else {
+      this.rotate(this.zoomDirection);
+    }
+  }
+
+  private zoomIn(): void {
+    if (this.width < this.zoomMax) {
+      this.width += 45;
+      this.zoom(this.width);
+    }
+  }
+
+  private zoomOut(): void {
+    if (this.width > this.zoomMin) {
+      this.width -= 45;
+      this.zoom(this.width);
+    }
+  }
+
+  private zoom(width: number): void {
+    this.zoomTarget.style.width = `${width}px`;
+  }
+
   private rotate(direction){
     let currentRotation = parseInt(this.zoomTarget.style.transform.replace('rotate(', '').replace('deg)', '')) || 0;
     const rotationIncrement = direction === 'rotate-left' ? 90 : (direction === 'rotate-right')? -90 : false;
+    this.zoomTarget.style.transformOrigin = "center center";
 
     if (rotationIncrement !== false ) {
+      console.log(this.zoomTarget.style.transformOrigin);
+
       const newRotation = currentRotation + rotationIncrement;
       this.zoomTarget.style.transform = `rotate(${newRotation}deg)`;
     }
